53995d8f2eb2844940cc3c0c53483112792e5d79
党留辉 <liuhuidang@sf-express.com>
2018-12-12 15:08:04 +0800

    修改bug

--- tests/task1.php
+++ tests/task1.php
@@ -22,12 +22,18 @@ include_once $path . '/../vendor/autoload.php';
         // 做自己的业务逻辑处理
         $this->log(microtime(true));
         $this->log(__FILE__);
-        sleep(mt_rand(2,5));
+        sleep(mt_rand(2, 5));
         $this->log(microtime(true));
     }
 
     private function log($str)
     {
         $this->trace($str);
+    }
+
+    protected function trace($msg)
+    {
+        $msg = sprintf('Task name [%s] Process [%d] pid [%d] : %s', $this->_receive_data['_task_name'], $this->_receive_data['_process_id'], getmypid(), $msg);
+        echo $msg . PHP_EOL;
     }
 })->run();
--- tests/test1.php
+++ tests/test1.php
@@ -18,19 +18,16 @@ try {
 }
 /*
 [work@gz-development01 test]$ ../php/bin/php ./tests/test1.php
-BaAGee\ProcessTask Process [1] Ready to start. /home/work/php/bin/php /home/work/test/tests/task1.php --name=小明 --age=20 --_task_name=task1 --_process_id=1 &
-BaAGee\ProcessTask Process [2] Ready to start. /home/work/php/bin/php /home/work/test/tests/task1.php --name=小明 --age=20 --_task_name=task1 --_process_id=2 &
-[work@gz-sems-development01 test]$ Task name [task1] Process [1] : Start time=2018-12-11 17:30:56
-Task name [task1] Process [1] : Receive data:{"name":"小明","age":"20","_task_name":"task1","_process_id":"1"}
-Task name [task1] Process [1] : 1544520656.9348
-Task name [task1] Process [1] : /home/work/test/tests/task1.php
-Task name [task1] Process [2] : Start time=2018-12-11 17:30:56
-Task name [task1] Process [2] : Receive data:{"name":"小明","age":"20","_task_name":"task1","_process_id":"2"}
-Task name [task1] Process [2] : 1544520656.9376
-Task name [task1] Process [2] : /home/work/test/tests/task1.php
-Task name [task1] Process [2] : 1544520658.9377
-Task name [task1] Process [2] : Over time=2018-12-11 17:30:58 used=2.000228
-Task name [task1] Process [1] : 1544520661.9349
-Task name [task1] Process [1] : Over time=2018-12-11 17:31:01 used=5.000236
-
+Task name [task1] Process [2] pid [92997] : Start time=2018-12-12 07:06:15
+Task name [task1] Process [2] pid [92997] : Receive data:{"name":"小明","age":"20","_task_name":"task1","_process_id":"2"}
+Task name [task1] Process [1] pid [92995] : Start time=2018-12-12 07:06:15
+Task name [task1] Process [2] pid [92997] : 1544598375.2569
+Task name [task1] Process [2] pid [92997] : /Users/baagee/PhpstormProjects/github/process-task/tests/task1.php
+Task name [task1] Process [1] pid [92995] : Receive data:{"name":"小明","age":"20","_task_name":"task1","_process_id":"1"}
+Task name [task1] Process [1] pid [92995] : 1544598375.2569
+Task name [task1] Process [1] pid [92995] : /Users/baagee/PhpstormProjects/github/process-task/tests/task1.php
+Task name [task1] Process [1] pid [92995] : 1544598377.2588
+Task name [task1] Process [1] pid [92995] : Over time=2018-12-12 07:06:17 used=2.002030
+Task name [task1] Process [2] pid [92997] : 1544598380.2578
+Task name [task1] Process [2] pid [92997] : Over time=2018-12-12 07:06:20 used=5.001051
 */
